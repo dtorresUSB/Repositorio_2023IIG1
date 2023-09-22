5455974f1ec3b218904d8c6290f68aadd80d0098
dtorresUSB <dtorres@usbbog.edu.co>
2023-09-22 13:08:42 -0500

    actualizacion 13

--- Corte2/Sesion13/Formato.py
+++
@@ -1,7 +0,0 @@
-def lectura():
-    f=open('wcm.csv','r',encoding="utf8")
-    lectura=f.readlines()
-    datos=[]
-    for i in lectura:
-        datos.append(i.rstrip('\n').split(','))
-    return datos
